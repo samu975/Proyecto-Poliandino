3f74c74851f7658c791bc50e95d9c45dbe881111
Samu Ross <safero99@gotmail.com>
2022-06-19 12:58:08 -0500

    Terminado el header y el menu para moviles, faltan queries para desktop y tablets

---
+++ main.js
@@ -0,0 +1,11 @@
+import './styles/style.css'
+
+
+var burgerMenu = document.getElementById('burger-menu');
+
+var overlay = document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
